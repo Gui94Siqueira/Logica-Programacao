377d0336e47adcbf144d4ea94be0fad5b7f82e78
Guilherme <guilherme94siqueira@gmail.com>
2024-01-23 20:34:00 -0300

    feat: implementação da logica Bibliografica

--- aulas/projetoBiblioteca/script.js
+++ aulas/projetoBiblioteca/script.js
@@ -4,6 +4,24 @@ class MaterialBibliografica {
         this.autor = autor;
         this.disponivel = true;
     }
+
+    realizarEmpresimo() {
+        if (this.disponivel) {
+            this.disponivel = false;
+            return true; // Emprestimo realizado com sucesso
+        } else {
+            return false; // Material já emprestado
+        }
+    }
+
+    realzarDevolucao() {
+        if(!this.disponivel) {
+            this.disponivel = true;
+            return true; // Livro devolvido com sucesso
+        } else {
+            return false; // Material já devolvido previamente
+        }
+    }
 }
 
 class Livro extends MaterialBibliografica {
